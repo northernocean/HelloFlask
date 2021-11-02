59cfe5384399c747b68f384b0e01081ec5113650
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-11-01 21:17:58 -0400

    add csv scraper

--- data/import.py
+++ data/import.py
@@ -12,7 +12,7 @@ def main():
     # -------------------------------------------------------------------
     if(import_method == 1):
 
-        con = sqlite3.connect('DAT.sqlite3') # database file input
+        con = sqlite3.connect('data/DAT.sqlite3') # database file input
         cur = con.cursor()
         cur.executescript("""
             DROP TABLE IF EXISTS earthquakes;
@@ -27,7 +27,7 @@ def main():
                 );
             """) # checks to see if table exists and makes a fresh table.
 
-        with open('database.csv', "r") as f: # CSV file input
+        with open('data/database.csv', "r") as f: # CSV file input
             reader = csv.reader(f, delimiter=',') # no header information with delimiter
             next(reader)
             for row in reader:
@@ -64,14 +64,22 @@ def main():
         # did not find infer_datetime_format=True to work correctly,
         # so importing the dates as strings and converting to
         # to dates use the pd.to_datetime method
-        df = pd.read_csv('database.csv', infer_datetime_format=False)
+        df = pd.read_csv('data/database.csv', infer_datetime_format=False)
         df['Date'] = pd.to_datetime(df['Date'], format="%m/%d/%Y")
         df['Time'] = pd.to_datetime(df['Time'], format="%H:%M:%S")
         
-        con = sqlite3.connect('DAT.sqlite3') # database file input
+        con = sqlite3.connect('data/DAT.sqlite3') # database file input
         df.to_sql('earthquakes', con, if_exists='replace', index=False)
         con.close()
 
+def reduce_db():
+    with open('data/database.csv', "r") as f: # CSV file input
+        reader = csv.reader(f, delimiter=',') # no header information with delimiter
+        with open('data/database2.csv', 'w') as f:
+            writer = csv.writer(f, delimiter=',') 
+            for i, row in enumerate(reader):
+                if i % 3 == 0:
+                    writer.writerow(row)
 
 if __name__=='__main__':
     main()
