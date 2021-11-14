c07181a661afebabbc65d2d4f118b831fd77717f
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-11-14 16:25:15 -0500

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # HelloFlask
 
-A sample flask application exhibiting data access techniques for small projects with various deployment options.
+A sample flask application exhibiting data access techniques for small heroku-deployed projects with various datasources such as sqlite, csv, json, or postgres.
 
 Dataset(s) used:
 [earthquake data](https://www.kaggle.com/usgs/earthquake-database)
--- data/switch_dbaccess_script.py
+++ data/switch_dbaccess_script.py
@@ -4,7 +4,10 @@ from datetime import datetime
 
 # this is a utility script that can be run directly as a python
 # file for copying a given dbaccess file into the project root
-# as dbaccess.py
+# as dbaccess.py. In order to avoid losing any changes that might
+# have been made directly in the current dbacccess.py file, it
+# also is set up to create backups of that file before replacing
+# it with the new version from this folder.
 
 # Setup
 root_dir = '/home/david/bootcamp/repos/HelloFlask'
