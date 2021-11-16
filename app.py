2c6796218f11db47b179e4dec812748dc0881802
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-11-15 22:00:57 -0500

    add earthquake route

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def index():
     xs, ys = db.get_earthquake_count_by_years()
     return render_template('index.html', data={'xs': xs, 'ys': ys, 'data_source': db.DATA_SOURCE})
 
+@app.route("/api/earthquakes")
+def earthquakes():
+    return "it works"
+
 if __name__ == "__main__":
     app.run()
 
