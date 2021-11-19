71a86b2c9e392bd1a41d72b896d6fcd0fed5847d
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-11-18 21:04:02 -0400

    edit app.py

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ import dbaccess as db
 load_dotenv()
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     xs, ys = db.get_earthquake_count_by_years()
-    return render_template('index.html', data={'xs': xs, 'ys': ys, 'data_source': db.DATA_SOURCE})
+    return render_template(
+        'index.html',
+        data={'xs': xs, 'ys': ys, 'data_source': db.DATA_SOURCE})
+
 
 @app.route("/api/earthquakes")
 def earthquakes():
